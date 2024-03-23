110cfa5a58959bf20bc5f98d2f2d09abf9c6c0a7
EASIRMAHI <136728953+EASIRMAHI@users.noreply.github.com>
2024-03-24 00:05:16 +0600

    Create Call boombering.js

---
+++ scripts/commands/Call boombering.js
@@ -0,0 +1,27 @@
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android" xmlns:app="http://schemas.android.com/apk/res-auto" xmlns:tools="http://schemas.android.com/tools" android:layout_width="match_parent" android:layout_height="match_parent" android:orientation="vertical">
+<LinearLayout android:id="@+id/linear4" android:layout_width="match_parent" android:layout_height="match_parent" android:background="#313131" android:gravity="center_horizontal" android:orientation="horizontal">
+<LinearLayout android:id="@+id/linear_a" android:layout_width="match_parent" android:layout_height="match_parent" android:gravity="center_horizontal|center_vertical" android:orientation="horizontal">
+<LinearLayout android:id="@+id/linear_aaaaa" android:layout_width="360dp" android:layout_height="550dp" android:padding="8dp" android:gravity="center_horizontal" android:orientation="vertical">
+<LinearLayout android:id="@+id/line" android:layout_width="200dp" android:layout_height="60dp" android:padding="8dp" android:gravity="center_horizontal|center_vertical" android:orientation="horizontal">
+<TextView android:id="@+id/textview2" android:layout_width="wrap_content" android:layout_height="wrap_content" android:padding="8dp" android:text="sms bomber" android:textSize="14sp" android:textStyle="bold" android:textColor="#01FF25"/>
+</LinearLayout>
+<LinearLayout android:id="@+id/linear_aaaa" android:layout_width="300dp" android:layout_height="50dp" android:padding="8dp" android:gravity="center_vertical" android:orientation="horizontal">
+<EditText android:id="@+id/Number" android:layout_width="match_parent" android:layout_height="wrap_content" android:padding="8dp" android:textSize="12sp" android:textStyle="bold" android:textColor="#01FF25" android:hint="Enter Number" android:textColorHint="#01FF25"/>
+</LinearLayout>
+<LinearLayout android:id="@+id/linea" android:layout_width="100dp" android:layout_height="wrap_content" android:padding="8dp" android:orientation="horizontal"/>
+<LinearLayout android:id="@+id/linear_aaa" android:layout_width="300dp" android:layout_height="50dp" android:padding="8dp" android:gravity="center_vertical" android:orientation="horizontal">
+<EditText android:id="@+id/edittext3" android:layout_width="match_parent" android:layout_height="wrap_content" android:padding="8dp" android:textSize="12sp" android:textStyle="bold" android:textColor="#01FF25" android:hint="Amount" android:textColorHint="#01FF25"/>
+</LinearLayout>
+<LinearLayout android:id="@+id/linear" android:layout_width="100dp" android:layout_height="wrap_content" android:padding="8dp" android:orientation="horizontal"/>
+<LinearLayout android:id="@+id/linear_bar1" android:layout_width="300dp" android:layout_height="8dp" android:padding="8dp" android:orientation="horizontal"/>
+<TextView android:id="@+id/textview1" android:layout_width="wrap_content" android:layout_height="wrap_content" android:padding="8dp" android:text="𝐋𝐞𝐯𝐞𝐥: 𝐁𝐚𝐧𝐠𝐥𝐚𝐝𝐞𝐬𝐡" android:textSize="12sp" android:textStyle="bold" android:textColor="#01FF25"/>
+<LinearLayout android:id="@+id/linear_bar" android:layout_width="300dp" android:layout_height="8dp" android:padding="8dp" android:orientation="horizontal"/>
+<LinearLayout android:id="@+id/linear13" android:layout_width="20dp" android:layout_height="60dp" android:padding="8dp" android:orientation="horizontal"/>
+<LinearLayout android:id="@+id/linear_aa" android:layout_width="150dp" android:layout_height="50dp" android:padding="8dp" android:gravity="center_horizontal|center_vertical" android:orientation="horizontal">
+<TextView android:id="@+id/textview3" android:layout_width="wrap_content" android:layout_height="wrap_content" android:padding="8dp" android:text="Attack" android:textSize="12sp" android:textStyle="bold" android:textColor="#01FF25"/>
+</LinearLayout>
+<LinearLayout android:id="@+id/linear14" android:layout_width="20dp" android:layout_height="50dp" android:padding="8dp" android:orientation="horizontal"/>
+</LinearLayout>
+</LinearLayout>
+</LinearLayout>
+</LinearLayout>
